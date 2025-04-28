3b15440bb9ddf4894ab5dfaf11734b5aa23447f1
kahayonz <marcdaniel_cajayon@yahoo.com>
2025-04-28 14:12:30 +0800

    fully connected to mongodb

--- login-system/server.js
+++ login-system/server.js
@@ -21,3 +21,5 @@ app.listen(5001, () => console.log("Server running on port 5001"));
 
 const authRoutes = require("./routes/authRoutes");
 app.use("/auth", authRoutes);
+
+app.listen(5001, () => console.log('Server running on port 5001'));
